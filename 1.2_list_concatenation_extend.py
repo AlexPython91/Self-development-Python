679a9dd522707a0d24e273e07d24e5935742fbfe
alexhvorostov <hvorostov.alex1@gmail.com>
2022-02-09 16:54:06 +0400

    Added file list concatenation second option. Method extend

---
+++ 1.2_list_concatenation_extend.py
@@ -0,0 +1,14 @@
+# Напишите функцию, которая будет принимать списки чисел в произвольном количестве и конкатенировать их, 
+# возвращая общий список чисел.
+
+from random import randint
+
+first_numbers = ([randint(1, 30) for i in range(5)])
+print(first_numbers)
+second_numbers = ([randint(1, 30) for i in range(5)])
+print(second_numbers)
+
+def return_the_list(first_numbers, second_numbers):
+    first_numbers.extend(second_numbers)
+    return first_numbers
+print(return_the_list(first_numbers, second_numbers))
