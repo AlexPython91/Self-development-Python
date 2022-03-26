e3fd4516fd1858a6fb00d97db1a7bc6de78fd8e1
alexhvorostov <hvorostov.alex1@gmail.com>
2022-03-26 22:32:18 +0400

    Added task modulo sum calculation

---
+++ 13.Absolute_amount.py
@@ -0,0 +1,9 @@
+# Даны пять чисел a1, a2, a3, a4, a5 
+# Напишите программу, которая вычисляет сумму их модулей 
+
+a1 = abs(float(input()))
+a2 = abs(float(input()))
+a3 = abs(float(input()))
+a4 = abs(float(input()))
+a5 = abs(float(input()))
+print('Absolute amount = ', a1 + a2 + a3 + a4 + a5)
