369d24d5bcfc0cd0ad3e0b235a5368d4a6e821b8
alexhvorostov <hvorostov.alex1@gmail.com>
2022-04-03 21:10:08 +0400

    Fixed code

--- 23.No_zeros.py
+++ 23.No_zeros.py
@@ -1,7 +1,8 @@
 # Напишите программу, которая считывает 10 чисел и выводит произведение 
 # отличных от нуля чисел.
 
-n = int(input())
+n = int(input('Enter number: '))
+print(f'Enter {n} numbers: ')
 product_of_numbers = 1
 for i in range(1, n + 1):
     numbers = int(input())
@@ -9,13 +10,3 @@ for i in range(1, n + 1):
         product_of_numbers = product_of_numbers * numbers
 print(product_of_numbers)
 
-# 8
-# 0
-# 1
-# 2
-# 1
-# 0
-# 0
-# 5
-# 4
-# 12
